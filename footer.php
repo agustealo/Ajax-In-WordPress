f69dd6c8a4b63f82e7a76d59db96de057a182d4a
Agustealo Johnson <agustealo@gmail.com>
2026-09-02 00:29:24 -0400

    fix: make footer own document close

--- footer.php
+++ footer.php
@@ -1,26 +1,29 @@
-    </div><!-- #content -->
-
 <?php
-$footer_layout = get_theme_mod('ajaxinwp_footer_layout', 'd-block');
+/**
+ * Theme footer.
+ *
+ * @package AjaxInWP
+ */
+
+$footer_layout = get_theme_mod( 'ajaxinwp_footer_layout', 'd-block' );
+$footer_text   = get_theme_mod( 'ajaxinwp_footer_text', '' );
 ?>
-    <footer id="colophon" class="site-footer <?php echo esc_attr($footer_layout); ?>" role="contentinfo">
-        <div class="site-info row">
-            <?php
-            $footer_text = get_theme_mod('ajaxinwp_footer_text', '');
-            if (!empty($footer_text)) {
-                echo '<span class="footer-text col-auto">' . esc_html($footer_text) . '</span>';
-            } else {
-                echo '<a href="' . esc_url(__('https://wordpress.org/', 'ajaxinwp')) . '" class="col-auto">' . sprintf(esc_html__('Proudly powered by %s', 'ajaxinwp'), 'WordPress') . '</a>';
-            }
-            ?>
-            <!-- Developer's copyright -->
-            <span class="col-auto">
-                &copy; <?php echo date('Y'); ?> <a href="https://agustealo.com" target="_blank">Zeus Eternal (Agustealo)</a>. All rights reserved.
-            </span>
-        </div><!-- .site-info -->
-    </footer><!-- #colophon -->
+<footer id="colophon" class="site-footer <?php echo esc_attr( $footer_layout ); ?>">
+	<div class="site-info row">
+		<?php if ( ! empty( $footer_text ) ) : ?>
+			<span class="footer-text col-auto"><?php echo esc_html( $footer_text ); ?></span>
+		<?php else : ?>
+			<a href="https://wordpress.org/" class="col-auto"><?php esc_html_e( 'Proudly powered by WordPress', 'ajaxinwp' ); ?></a>
+		<?php endif; ?>
+
+		<span class="col-auto">
+			&copy; <?php echo esc_html( wp_date( 'Y' ) ); ?>
+			<a href="https://agustealo.com/" target="_blank" rel="noopener noreferrer">Zeus Eternal (Agustealo)</a>.
+			<?php esc_html_e( 'All rights reserved.', 'ajaxinwp' ); ?>
+		</span>
+	</div>
+</footer>
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
